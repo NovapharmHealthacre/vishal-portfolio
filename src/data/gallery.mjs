af7d7babf85253e595078f89c8561507be8d320b
novapharmhealthcare <vishal@novapharmhealthcare.com>
2026-07-15 16:07:56 +0530

    content: initialise founder gallery data

---
+++ src/data/gallery.mjs
@@ -0,0 +1,9 @@
+export const galleryMeta = Object.freeze({
+  name: 'Vishal Chakravarty Portrait Gallery',
+  description: 'A curated portrait gallery of Vishal Chakravarty, Founder and CEO of NovaPharm Healthcare Ltd.',
+  path: '/gallery/',
+});
+
+const image = (record) => Object.freeze(record);
+
+export const galleryImages = Object.freeze([]);
